397c5ae0369c669810cbeb6134473e6684e88199
thisisgokul <gokulgs4923@gmail.com>
2024-11-30 17:16:38 +0530

    testing

--- app/add-vehicles/page.jsx
+++ app/add-vehicles/page.jsx
@@ -1,12 +1,18 @@
-"use client"
+'use client'
+
 import AddProducts from "@/components/shared/dashboard-common/AddProducts";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function page() {
+export default function Page() {
   const searchParams = useSearchParams();
   const id = searchParams.get('id');
 
   return (
-   <div><AddProducts id={id}/></div>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AddProducts id={id} />
+      </Suspense>
+    </div>
   );
 }
